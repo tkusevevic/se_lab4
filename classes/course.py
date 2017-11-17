c8dfb4272cfdacf86c54bb4fb7c448b5bfd5a1ec
kusevic <tkusevic@gmail.com>
2017-11-17 15:41:50 +0100

    add new @classmethod in class course

--- classes/course.py
+++ classes/course.py
@@ -9,6 +9,30 @@ class Course:
         self.runnings.append(CourseRunning(self, year))
         return self.runnings[-1]
 
+    @classmethod
+    def get_courses_from_file(self,filename):
+        #opening file and reading
+        with open (filename, "r") as f:
+            lines = f.readlines()
+
+
+            data = []
+            #removing # and triming strings
+            for a in lines:
+                if not a.startswith('#'):
+                    data.append(a.strip())
+
+            courses=[]
+            #for every string if starts with Course createing Course object
+            #if starts with Running create Running course
+            for d in data:
+                x = d.split(' | ')
+                if "Course" in x[0]:
+                    courses.append(Course(x[1],x[2]))
+                elif "Running" in x[0]:
+                    courses[-1].add_running(int(x[1]))
+        return courses
+
 class CourseRunning:
     def __init__(self, course, year):
         self.course = course
