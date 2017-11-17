70746dd0beb04166ce20af8713dabefac616428e
kusevic <tkusevic@gmail.com>
2017-11-17 15:09:17 +0100

    change the main program

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from classes.course import Course
 from classes.student import Student
 
 
-pi = Course("Programsko inzenjerstvo", "SARS503")
-pi2017 = pi.add_running(2017)
-pi2016 = pi.add_running(2016)
-print pi
-mat = Course("Matematika 1", "SER101")
-mat.add_running(2017)
-mat.add_running(2016)
-print mat
+with open ("courses.txt", "r") as f:
+    lines = f.readlines();
 
 
+data = []
+for a in lines:
+    if not a.startswith('#'):
+        data.append(a.strip())
+
+courses=[]
+
+for d in data:
+    x = d.split(' | ')
+    if "Course" in x[0]:
+        courses.append(Course(x[1],x[2]))
+    elif "Running" in x[0]:
+        courses[-1].add_running(int(x[1]))
+
+for a in courses:
+    print a
